1293d992962ce40b219b61d48f30f0f79f0e780d
Zach Laine <whatwasthataddress@gmail.com>
2018-11-13 16:46:28 -0600

    Update, dropping std:: overloads and find_not_*().

--- synopsis.hpp
+++ synopsis.hpp
@@ -1,59 +1,46 @@
-namespace std {
+namespace std { namespace ranges {
 
-template<typename BidirectionalIterator, typename T>
-constexpr BidirectionalIterator find_backward(BidirectionalIterator first,
-                                              BidirectionalIterator last,
-                                              const T & value);
+    template<InputIterator I, class T, class Proj = identity>
+    requires IndirectRelation<
+        ranges::equal_to<>,
+        projected<I, Proj>,
+        const T*>
+    constexpr I
+    find_backward(I first, I last, const T& value, Proj proj = {});
 
-template<typename BidirectionalIterator, typename T>
-constexpr BidirectionalIterator find_not_backward(BidirectionalIterator first,
-                                                  BidirectionalIterator last,
-                                                  const T & value);
+    template<InputRange R, class T, class Proj = identity>
+    requires IndirectRelation<
+        ranges::equal_to<>,
+        projected<iterator_t<R>, Proj>,
+        const T*>
+    constexpr safe_iterator_t<R>
+    find_backward(R&& r, const T& value, Proj proj = {});
 
-template<typename BidirectionalIterator, typename Pred>
-constexpr BidirectionalIterator find_if_backward(BidirectionalIterator first,
-                                                 BidirectionalIterator last,
-                                                 Pred p);
+    template<
+        InputIterator I,
+        class Proj = identity,
+        IndirectUnaryPredicate<projected<I, Proj>> Pred>
+    constexpr I find_if_backward(I first, I last, Pred pred, Proj proj = {});
 
-template<typename BidirectionalIterator, typename Pred>
-constexpr BidirectionalIterator find_if_not_backward(BidirectionalIterator first,
-                                                     BidirectionalIterator last,
-                                                     Pred p);
+    template<
+        InputRange R,
+        class Proj = identity,
+        IndirectUnaryPredicate<projected<iterator_t<R>, Proj>> Pred>
+    constexpr safe_iterator_t<R>
+    find_if_backward(R&& r, Pred pred, Proj proj = {});
 
-template<typename InputIterator, typename T>        
-constexpr InputIterator find_not(InputIterator first, InputIterator last, const T & value);
+    template<
+        InputIterator I,
+        class Proj = identity,
+        IndirectUnaryPredicate<projected<I, Proj>> Pred>
+    constexpr I
+    find_if_not_backward(I first, I last, Pred pred, Proj proj = {});
 
-namespace ranges {
+    template<
+        InputRange R,
+        class Proj = identity,
+        IndirectUnaryPredicate<projected<iterator_t<R>, Proj>> Pred>
+    constexpr safe_iterator_t<R>
+    find_if_not_backward(R&& r, Pred pred, Proj proj = {});
 
-    template<BidirectionalRange Rng, class T, class Proj = identity>
-    constexpr requires IndirectRelation<ranges::equal_to<>, projected<iterator_t<Rng>, Proj>, const T *>
-    safe_iterator_t<Rng>
-      find_backward(Rng && rng, const T & value, Proj proj = Proj{});
-
-    template<BidirectionalRange Rng, class T, class Proj = identity>
-    constexpr requires IndirectRelation<ranges::equal_to<>, projected<iterator_t<Rng>, Proj>, const T *>
-    safe_iterator_t<Rng>
-      find_not_backward(Rng && rng, const T & value, Proj proj = Proj{});
-
-    template <InputRange Rng, class Proj = identity,
-              IndirectUnaryPredicate<projected<iterator_t<Rng>, Proj>> Pred>
-    constexpr safe_iterator_t<Rng>
-      find_if_backward(Rng&& rng, Pred pred, Proj proj = Proj{});
-
-    template <InputRange Rng, class Proj = identity,
-              IndirectUnaryPredicate<projected<iterator_t<Rng>, Proj>> Pred>
-    constexpr safe_iterator_t<Rng>
-      find_if_not_backward(Rng&& rng, Pred pred, Proj proj = Proj{});
-
-    template<InputIterator I, Sentinel<I> S, class T, class Proj = identity>
-    constexpr requires IndirectRelation<ranges::equal_to<>, projected<I, Proj>, const T *>
-    I find_not(I first, S last, const T & value, Proj proj = Proj{});
-
-    template<InputRange Rng, class T, class Proj = identity>
-    constexpr requires IndirectRelation<ranges::equal_to<>, projected<iterator_t<Rng>, Proj>, const T *>
-    safe_iterator_t<Rng>
-      find(Rng && rng, const T & value, Proj proj = Proj{});
-
-}
-
-}
+}}
